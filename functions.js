dd01559a64a10435e6433aecc76798dbfcd0149e
pugaIsaias <64232550+pugaIsaias@users.noreply.github.com>
2021-03-08 21:59:32 -0600

    add function using axios GET

--- functions.js
+++ functions.js
@@ -1,3 +1,5 @@
+const { default: axios } = require("axios");
+
 const functions = {
   add: (num1, num2) => num1 + num2,
   isNull: () => null,
@@ -7,6 +9,11 @@ const functions = {
     user["lastName"] = "Puga";
     return user;
   },
+  fetchUser: () =>
+    axios
+      .get("https://jsonplaceholder.typicode.com/users/1")
+      .then((res) => res, data)
+      .catch((err) => "error"),
 };
 
 module.exports = functions;
