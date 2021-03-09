9fc8c012f44908ae821adb1bf24e2e0ef62e4cbc
pugaIsaias <64232550+pugaIsaias@users.noreply.github.com>
2021-03-08 20:12:31 -0600

    add unit test for isNull

--- functions.test.js
+++ functions.test.js
@@ -7,3 +7,7 @@ test("Add 2+ 2 to equals 4", () => {
 test("Add 2+2 to NOT equals 5", () => {
   expect(functions.add(2, 2)).not.toBe(5);
 });
+
+test("Should be null", () => {
+  expect(functions.isNull()).toBeNull();
+});
