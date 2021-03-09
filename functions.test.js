c225ac994c4bc1759dfe8bb66a8f8ac825a537ad
pugaIsaias <64232550+pugaIsaias@users.noreply.github.com>
2021-03-08 20:09:38 -0600

    add a Not to be test for Add

--- functions.test.js
+++ functions.test.js
@@ -3,3 +3,7 @@ const functions = require("./functions");
 test("Add 2+ 2 to equals 4", () => {
   expect(functions.add(2, 2)).toBe(4);
 });
+
+test("Add 2+2 to NOT equals 5", () => {
+  expect(functions.add(2, 2)).not.toBe(5);
+});
