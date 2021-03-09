23ec2e7360c7bd5ac61ad370171e3844481d844d
pugaIsaias <64232550+pugaIsaias@users.noreply.github.com>
2021-03-08 21:14:34 -0600

    add test less and equal to

--- functions.test.js
+++ functions.test.js
@@ -24,3 +24,16 @@ test("User should be Isaias Puga", () => {
     lastName: "Puga",
   });
 });
+
+//Less than and greater than
+test("Should be under 1600", () => {
+  const load1 = 800;
+  const load2 = 700;
+  expect(load1 + load2).toBeLessThan(1600);
+});
+
+test("Should be under or equal 1600", () => {
+  const load1 = 800;
+  const load2 = 800;
+  expect(load1 + load2).toBeLessThanOrEqual(1600);
+});
