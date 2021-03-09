cc5e8c5eeb7384d2179793ac3de3bedbe434c948
pugaIsaias <64232550+pugaIsaias@users.noreply.github.com>
2021-03-08 21:13:37 -0600

    add createUser function

--- functions.js
+++ functions.js
@@ -2,6 +2,11 @@ const functions = {
   add: (num1, num2) => num1 + num2,
   isNull: () => null,
   checkValue: (x) => x,
+  createUser: () => {
+    const user = { firstName: "Isaias" };
+    user["lastName"] = "Puga";
+    return user;
+  },
 };
 
 module.exports = functions;
