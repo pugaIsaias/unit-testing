8baa60a55b2b3c7b8e12a38accf642143efe270f
pugaIsaias <64232550+pugaIsaias@users.noreply.github.com>
2021-03-08 20:20:37 -0600

    add unit test for falsy

--- functions.test.js
+++ functions.test.js
@@ -11,3 +11,7 @@ test("Add 2+2 to NOT equals 5", () => {
 test("Should be null", () => {
   expect(functions.isNull()).toBeNull();
 });
+
+test("Should be falsy", () => {
+  expect(functions.check(null)).toBeFalsy();
+});
